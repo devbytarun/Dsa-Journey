179202b4b28619f8600fb1351448fbf540b0bd27
devbytarun <devbytarun@gmail.com>
2026-06-11 20:11:50 +0530

    Complete print statements basics

---
+++ Java_Basics/PrintStatements.java
@@ -0,0 +1,10 @@
+package Java_Basics;
+
+public class PrintStatements {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Tarun");
+        System.out.println("Welcome to DSA Journey");
+
+    }
+}
