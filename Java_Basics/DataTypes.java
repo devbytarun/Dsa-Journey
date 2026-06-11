1f8b08c10d2c0d53a1e16d9e7263cc7059467e91
devbytarun <devbytarun@gmail.com>
2026-06-11 20:17:32 +0530

    Complete data types basics

---
+++ Java_Basics/DataTypes.java
@@ -0,0 +1,20 @@
+package Java_Basics;
+
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        int age = 19;
+        double weight = 90.5;
+        char grade = 'A';
+        boolean isStudent = true;
+        String name = "Tarun";
+
+        System.out.println(age);
+        System.out.println(weight);
+        System.out.println(grade);
+        System.out.println(isStudent);
+        System.out.println(name);
+
+    }
+}
