2f7a5a40ada928d097187dd2433b21d70c915486
devbytarun <devbytarun@gmail.com>
2026-06-11 19:40:24 +0530

    Initial commit

---
+++ Java_Basics/Day1.java
@@ -0,0 +1,10 @@
+package Java_Basics;
+
+public class Day1 {
+    public static void main(String[] args) {
+        int age = 19;
+        String name = "Tarun";
+        System.out.println(name);
+        System.out.println(age);
+    }
+}
