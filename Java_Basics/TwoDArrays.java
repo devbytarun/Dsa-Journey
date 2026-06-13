eb3e7460ba12147bb9f7c73a449a0c819b21a1c7
devbytarun <devbytarun@gmail.com>
2026-06-13 16:03:55 +0530

    Complete 2D arrays basics

---
+++ Java_Basics/TwoDArrays.java
@@ -0,0 +1,29 @@
+package Java_Basics;
+
+public class TwoDArrays {
+
+    public static void main(String[] args) {
+
+        int[][] numbers = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        System.out.println(numbers[0][0]);
+
+        System.out.println(numbers[1][2]);
+
+        System.out.println("Full Array:");
+
+        for (int i = 0; i < numbers.length; i++) {
+
+            for (int j = 0; j < numbers[i].length; j++) {
+
+                System.out.print(numbers[i][j] + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
