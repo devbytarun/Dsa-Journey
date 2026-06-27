94b3947fa6e69a966f3e928ff586dd1233370590
devbytarun <devbytarun@gmail.com>
2026-06-27 07:11:17 +0530

    Solve second largest number in array

---
+++ DSA/Arrays/SecondLargest.java
@@ -0,0 +1,36 @@
+package DSA.Arrays;
+
+public class SecondLargest {
+
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 5, 99, 45};
+
+        int largest;
+        int secondLargest;
+
+        if (arr[0] > arr[1]) {
+            largest = arr[0];
+            secondLargest = arr[1];
+        } else {
+            largest = arr[1];
+            secondLargest = arr[0];
+        }
+
+        for (int i = 2; i < arr.length; i++) {
+
+            if (arr[i] > largest) {
+
+                secondLargest = largest;
+                largest = arr[i];
+
+            } else if (arr[i] > secondLargest) {
+
+                secondLargest = arr[i];
+            }
+        }
+
+        System.out.println("Largest = " + largest);
+        System.out.println("Second Largest = " + secondLargest);
+    }
+}
