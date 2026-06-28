1a1d305d8c7acf3203f5998eebd47b92cb2be4a2
devbytarun <devbytarun@gmail.com>
2026-06-28 07:24:50 +0530

    Count positive negative and zero in array

---
+++ DSA/Arrays/CountPositiveNegative.java
@@ -0,0 +1,33 @@
+package DSA.Arrays;
+
+public class CountPositiveNegative {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {-1, 2, 5, -6, 0};
+
+        int positiveCount = 0;
+        int negativeCount = 0;
+        int zeroCount = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+
+            if (numbers[i] < 0) {
+
+                negativeCount++;
+
+            } else if (numbers[i] > 0) {
+
+                positiveCount++;
+
+            } else {
+
+                zeroCount++;
+            }
+        }
+
+        System.out.println("Positive = " + positiveCount);
+        System.out.println("Negative = " + negativeCount);
+        System.out.println("Zero = " + zeroCount);
+    }
+}
