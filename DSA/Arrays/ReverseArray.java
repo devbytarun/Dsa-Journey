67d791e4d82d51c7be7d5d25e55adca9b86c6bb5
devbytarun <devbytarun@gmail.com>
2026-06-30 02:35:12 +0530

    Complete array foundation for DSA

--- DSA/Arrays/ReverseArray.java
+++ DSA/Arrays/ReverseArray.java
@@ -1,10 +1,28 @@
 package DSA.Arrays;
 
 public class ReverseArray {
+
     public static void main(String[] args) {
-        int[] numbers = { 10, 20, 30, 40, 50 };
-        for (int i = numbers.length - 1; i >= 0; i--) {
-            System.out.println(numbers[i]);
+
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left < right) {
+
+            int temp = numbers[left];
+            numbers[left] = numbers[right];
+            numbers[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        System.out.print("Reversed Array: ");
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
         }
     }
 }
